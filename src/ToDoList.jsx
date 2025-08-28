a0d395663cb1c4580a7f10c9fbcb1f3484d28b51
SergejNov <sergejnovakovic55@gmail.com>
2025-08-28 20:39:03 +0200

    Added functionality, delete button and styling, fixed a lot of bugs during development

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -4,15 +4,36 @@ import './ToDo.css';
 export default function ToDoList(){
     const [todos, setTodos] = useState([
   { id: 1, text: "Task 1", completed: true },
-  { id: 2, text: "Task 2", completed: false }
-]);
+  { id: 2, text: "Task 2", completed: false },
+])
 
- const [newTodos, setnewTodos] = useState("");
+ const [newTodo, setnewTodo] = useState("");
 
 
  const handleInputChange = (e)=>{
-    setnewTodos(e.target.value);
+    setnewTodo(e.target.value);
 }
+
+ const addTask = ()=> {
+    if(newTodo.trim() !== ''){
+        setTodos([...todos, 
+        {id: Date.now(), text: newTodo, completed: false}
+        ])
+        setnewTodo('');
+
+    }
+ }
+
+const toggleComplete = (id)=>{
+    setTodos(todos.map(todo =>
+        todo.id === id ? {...todo, completed: !todo.completed}: todo
+    ))
+}
+
+  const handleDelete = (id)=>{
+    setTodos(todos.filter(todo => todo.id !== id))
+
+  }
 
     return (<>
     <div className='main-container'>
@@ -21,15 +42,17 @@ export default function ToDoList(){
         <ul>
             {
             todos.map((item, id)=>{
-               return <li key={id}>{item.text}</li>     
+               return <li key={id} className={`task-li ${item.completed && 'completed-task'}`} > <span onClick={()=> toggleComplete(item.id)} className='task-text'>{item.text}</span>
+               <button className='delete-button' onClick={()=>handleDelete(item.id)}>Delete</button>
+               </li>     
             })
             }
         </ul>
     </div>
     <div className='input-container'>
         <h2>Add tasks to the To-do List.</h2>
-        <input type="text" placeholder="Feed the chickens" onChange={handleInputChange} />
-        <button>Add task</button>
+        <input className='input-field' type="text" placeholder="Feed the chickens" value={newTodo} onChange={handleInputChange} />
+        <button className='add-button' onClick={addTask}>Add task</button>
     </div>
     </div>
     </>);
